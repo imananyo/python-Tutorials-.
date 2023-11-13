8df1df8dde84353f25f3cef5ee456e1a1cf9a5b9
Ananyo Dutta <132311358+imananyo@users.noreply.github.com>
2023-11-14 03:03:37 +0530

    Add files via upload

---
+++ day1-learning-python - Copy.py
@@ -0,0 +1,94 @@
+print('hello world !')
+print('my name is ananyo \n I work in pwskills ')
+a = 10
+print(a)
+a
+print('hey there \n I am ananyo ')
+
+# Variables Assignment
+name = 'ananyo'
+number = 10
+name
+number 
+name = 'Alakh'
+number = 20 
+number 
+name
+type(name)
+type(number)
+
+#  Float / Decimal number 
+
+decimal_num = 2.3
+type(decimal_num)
+
+# complex number 
+c1 = 2 + 5j
+type(c1)
+number1 = 23 
+company  = 'ineuron'
+company = 'pwskills'
+print(company)
+# so variable names are case sensitive . 
+#  reserved keywords = int , float , bool , str , complex ..
+
+# Bool 
+
+True
+type(True)
+False
+type(False)
+True and True
+False and False
+True or False
+False or True 
+True and False 
+type(not False)
+bool(0)
+bool(1)
+1 and False
+1 and 0
+1 or 0
+0 or 0
+0 and 1 
+0 or 1
+
+str(23)
+int('23')
+type(int('23'))
+name1 = 'krish'
+company = 'pwskills'
+name1
+company 
+type(name1)
+decimal_num = 2.5
+type(decimal_num)
+c = 1 + 2j
+type(c)
+
+# variable naming 
+# never use a number before a variable 
+number1 = 23 
+company = "ineuron "
+company = " pwskills"
+company
+# variable names are case sensitive 
+a = 1 
+if type(bool(a))==True :
+    print('True')
+
+bool(0)
+bool(1)
+# this is called typecasting 
+str(23)
+int('23')
+# dynamic typing 
+
+# you dont have to define data type 
+#  concatination between diff types 
+'1' + '1'
+int('1') + int('1')
+# int('1') + 1  diff data cant be concatinated 
+'1' * 100
+
+
